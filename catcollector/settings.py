0e8b1545ca7db748c81bc1b7161db6a1fc00a982
generalGreg <thompson.gregory.mark@gmail.com>
2025-10-18 16:50:46 -0700

    Finally finished this lab. I got waylaid because of Django.

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -117,7 +117,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+LOGIN_URL = 'home'
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Add this variable to specify where successful logins should redirect to
+LOGIN_REDIRECT_URL = 'cat-index'
+
+LOGOUT_REDIRECT_URL = 'home'
